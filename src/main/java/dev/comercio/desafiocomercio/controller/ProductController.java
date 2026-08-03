21381520de8f62db0d8399ca5e0be863a4d3b348
Douglas Machado <douglasvilla23@gmail.com>
2026-08-02 23:30:39 -0300

    Feat: cria productService

---
+++ src/main/java/dev/comercio/desafiocomercio/controller/ProductController.java
@@ -0,0 +1,20 @@
+package dev.comercio.desafiocomercio.controller;
+
+
+import dev.comercio.desafiocomercio.service.ProductService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    public 
+
+}
--- src/main/java/dev/comercio/desafiocomercio/service/ProductService.java
+++ src/main/java/dev/comercio/desafiocomercio/service/ProductService.java
@@ -1,4 +1,21 @@
 package dev.comercio.desafiocomercio.service;
 
+import dev.comercio.desafiocomercio.entity.Product;
+import dev.comercio.desafiocomercio.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product createProduct(Product product){
+        return productRepository.save(product);
+    }
+
+
 }
