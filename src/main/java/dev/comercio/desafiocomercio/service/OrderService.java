f960eb6f5d28652931fcbdb0748d882c98315cb4
Douglas Machado <douglasvilla23@gmail.com>
2026-08-06 23:46:06 -0300

    Feat: Cria o create, findAll e FindById para o OrderService

--- src/main/java/dev/comercio/desafiocomercio/service/OrderService.java
+++ src/main/java/dev/comercio/desafiocomercio/service/OrderService.java
@@ -1,4 +1,31 @@
 package dev.comercio.desafiocomercio.service;
 
+import dev.comercio.desafiocomercio.entity.Order;
+import dev.comercio.desafiocomercio.repository.OrderRepository;
+import jakarta.persistence.Entity;
+
+import java.util.List;
+import java.util.Optional;
+
+@Entity
 public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public Order createOrder(Order order){
+        return orderRepository.save(order);
+    }
+
+    public List<Order> findAll(){
+        return orderRepository.findAll();
+    }
+
+    public Optional<Order> findById(Long id){
+        return orderRepository.findById(id);
+    }
+
 }
